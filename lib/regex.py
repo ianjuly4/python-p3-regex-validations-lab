02b05d3fbcefd579fe0f33c55cae058969bad71a
Ian July <ianjuly4@gmail.com>
2024-04-07 12:19:40 -0600

    regex validations

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,11 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r"John Cena|Anya Taylor-Joy|D'Angelo" 
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"[5]{10}|[5]{3}\S[5]{3}\S[5]{4}|\S[5]{3}\S [5]{3}\S[5]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"^[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$|^[a-zA-Z]+\.[a-zA-Z]+@[a-zA-Z]+\.[a-zA-Z]+$|^[a-zA-Z]+\.[a-zA-z]+[0-9]+@[a-zA-Z]+\.[a-zA-z]+$"
 email_regex = re.compile(email_address)
